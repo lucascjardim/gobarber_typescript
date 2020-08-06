cf9ab5ce29ca0fb8b0b69440389a5c0384cd022f
Lucas Costa jardim <lucascostajardim@iMac-de-Lucas.local>
2020-08-05 22:29:42 -0300

    GoBarber 2020 Mobile - Final

--- appgobarber/src/hooks/auth.tsx
+++ appgobarber/src/hooks/auth.tsx
@@ -2,9 +2,16 @@ import React, { createContext, useCallback, useState, useContext, useEffect } fr
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: object;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -13,10 +20,11 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: object;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  updateUser(user: User): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -34,6 +42,7 @@ export const AuthProvider: React.FC = ({ children }) => {
         ]
       );
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -50,7 +59,7 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ])
-
+    api.defaults.headers.authorization = `Bearer ${token}`;
     setData({ token, user });
   }, []);
 
@@ -62,8 +71,17 @@ export const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState);
   }, []);
 
+  const updateUser = useCallback(async (user: User) => {
+    await AsyncStorage.setItem('@GoBarber:user', JSON.stringify(user));
+    setData({
+      token: data.token,
+      user,
+    })
+  }, [setData, data.token]);
+
   return (
-    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, loading, signIn, signOut, updateUser }}>
       {children}
     </AuthContext.Provider>
   );
